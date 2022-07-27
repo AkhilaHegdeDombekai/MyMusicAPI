6a454f8db755260c9417d0a6f586964f13773aac
AkhilaHegdeDombekai <akhilahegd155@gmail.com>
2022-07-27 20:42:55 +0530

    added songs repository skeleton

---
+++ MyMusic.Data/Repository/SongsRepository.cs
@@ -0,0 +1,45 @@
+﻿using MyMusic.Data.Helper.Interfaces;
+using MyMusic.Data.Model;
+using MyMusic.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Data.Repository
+{
+    public class SongsRepository:ISongsRepository
+    {
+        private IConnectionHelper _connectionHelper;
+
+        public SongsRepository(IConnectionHelper connectionHelper)
+        {
+            _connectionHelper = connectionHelper;
+        }
+
+        public Task<int> DeleteSongs(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Songs>> GetAllSongs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Songs> GetSongsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Songs> SaveSongs(Songs Artist)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Songs> UpdateSongs(Songs Artist)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
